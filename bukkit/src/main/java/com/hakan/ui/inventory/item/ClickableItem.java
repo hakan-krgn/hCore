b2d4aa03590210a60e3edfebffdf9d00b50d49f3
Hakan Kargın <hakankargin_2003@hotmail.com>
2022-03-12 19:52:32 +0300

    First commit

---
+++ bukkit/src/main/java/com/hakan/ui/inventory/item/ClickableItem.java
@@ -0,0 +1,28 @@
+package com.hakan.ui.inventory.item;
+
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Objects;
+import java.util.function.Consumer;
+
+public final class ClickableItem {
+
+    private final ItemStack item;
+    private final Consumer<InventoryClickEvent> click;
+
+    public ClickableItem(@Nonnull ItemStack item, @Nullable Consumer<InventoryClickEvent> click) {
+        this.item = Objects.requireNonNull(item, "item cannot be null!");
+        this.click = click;
+    }
+
+    public ItemStack getItem() {
+        return this.item;
+    }
+
+    public Consumer<InventoryClickEvent> getClick() {
+        return this.click;
+    }
+}
