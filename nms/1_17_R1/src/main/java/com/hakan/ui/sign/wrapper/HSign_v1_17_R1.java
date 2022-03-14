53a5bd1dc579fd5a5748b222c5c45837bb0342c3
Hakan Kargın <hakankargin_2003@hotmail.com>
2022-03-14 17:00:34 +0300

    Sign: - Added all version supports to sign api. - Fixed 1.18 support problem.

--- nms/1_17_R1/src/main/java/com/hakan/ui/sign/wrapper/HSign_v1_17_R1.java
+++ nms/1_17_R1/src/main/java/com/hakan/ui/sign/wrapper/HSign_v1_17_R1.java
@@ -8,18 +8,16 @@ import net.minecraft.network.chat.IChatBaseComponent;
 import net.minecraft.network.protocol.game.PacketPlayInUpdateSign;
 import net.minecraft.network.protocol.game.PacketPlayOutBlockChange;
 import net.minecraft.network.protocol.game.PacketPlayOutOpenSignEditor;
-import net.minecraft.server.network.PlayerConnection;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.TileEntitySign;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.craftbukkit.v1_18_R1.block.CraftSign;
-import org.bukkit.craftbukkit.v1_18_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_17_R1.block.CraftSign;
 import org.bukkit.entity.Player;
 
-public class HSign_v1_18_R1 extends HSign {
+public class HSign_v1_17_R1 extends HSign {
 
-    public HSign_v1_18_R1(Material type, String... lines) {
+    public HSign_v1_17_R1(Material type, String... lines) {
         super(type, lines);
     }
 
@@ -27,12 +25,12 @@ public class HSign_v1_18_R1 extends HSign {
     public void open(Player player) {
         BlockPosition blockPosition = new BlockPosition(player.getLocation().getBlockX(), 1, player.getLocation().getBlockZ());
 
-        HCore.sendPacket(player, new PacketPlayOutBlockChange(blockPosition, Blocks.cg.n()));
+        HCore.sendPacket(player, new PacketPlayOutBlockChange(blockPosition, Blocks.cg.getBlockData()));
 
         IChatBaseComponent[] components = CraftSign.sanitizeLines(this.lines);
-        TileEntitySign sign = new TileEntitySign(new BlockPosition(blockPosition.u(), blockPosition.v(), blockPosition.w()), Blocks.cg.n());
+        TileEntitySign sign = new TileEntitySign(new BlockPosition(blockPosition.getX(), blockPosition.getY(), blockPosition.getZ()), Blocks.cg.getBlockData());
         System.arraycopy(components, 0, sign.d, 0, sign.d.length);
-        HCore.sendPacket(player, sign.c());
+        HCore.sendPacket(player, sign.getUpdatePacket());
 
         HCore.sendPacket(player, new PacketPlayOutOpenSignEditor(blockPosition));
         HSignHandler.getContent().put(player.getUniqueId(), this);
@@ -43,7 +41,7 @@ public class HSign_v1_18_R1 extends HSign {
         PacketPlayInUpdateSign packetPlayInUpdateSign = (PacketPlayInUpdateSign) packet;
 
         BlockPosition position = packetPlayInUpdateSign.b();
-        Block block = player.getWorld().getBlockAt(position.u(), position.v(), position.w());
+        Block block = player.getWorld().getBlockAt(position.getX(), position.getY(), position.getZ());
         block.setType(block.getType());
 
         if (this.consumer != null)
