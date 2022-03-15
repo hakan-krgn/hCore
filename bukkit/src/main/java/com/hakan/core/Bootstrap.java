41bd58a47fcb374ea52ad93332a41a746810d3c7
Ahmet Hakan Kargın <hakankargin03@gmail.com>
2022-03-15 23:35:12 +0300

    - Removed unused docs.

--- bukkit/src/main/java/com/hakan/core/Bootstrap.java
+++ bukkit/src/main/java/com/hakan/core/Bootstrap.java
@@ -2,11 +2,6 @@ package com.hakan.core;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-/**
- * Main class of this core.
- * You can reach all APIs from this
- * class as static.
- */
 public class Bootstrap extends JavaPlugin {
 
     /*
