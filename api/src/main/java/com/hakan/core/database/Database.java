afc9dfb6f3ed7f4594571a4305d086cb56b8ad03
Ahmet Hakan Kargın <hakankargin03@gmail.com>
2022-03-18 00:13:07 +0300

    - Added javadocs to database api.

--- api/src/main/java/com/hakan/core/database/Database.java
+++ api/src/main/java/com/hakan/core/database/Database.java
@@ -1,13 +1,29 @@
 package com.hakan.core.database;
 
+/**
+ * Database class to implement
+ * databases.
+ *
+ * @param <T> Database object class.
+ */
 public abstract class Database<T extends DatabaseObject> {
 
     protected T t;
 
+    /**
+     * Creates new instance of this class.
+     *
+     * @param t Parent class.
+     */
     public Database(T t) {
         this.t = t;
     }
 
+    /**
+     * Gets parent class.
+     *
+     * @return Parent class.
+     */
     public T getParent() {
         return this.t;
     }
--- api/src/main/java/com/hakan/core/database/DatabaseObject.java
+++ api/src/main/java/com/hakan/core/database/DatabaseObject.java
@@ -1,4 +1,9 @@
 package com.hakan.core.database;
 
+/**
+ * DatabaseObject class to
+ * implement classes to be
+ * saved in the database
+ */
 public interface DatabaseObject {
 }
