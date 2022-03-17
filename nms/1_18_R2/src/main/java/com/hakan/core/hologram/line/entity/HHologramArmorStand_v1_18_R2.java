f9be280de8d5ec4549042b36ae9ca1edf5f3ea83
Ahmet Hakan Kargın <hakankargin03@gmail.com>
2022-03-17 21:34:40 +0300

    - Fixed compile problem.

--- nms/1_18_R2/src/main/java/com/hakan/core/hologram/line/entity/HHologramArmorStand_v1_18_R2.java
+++ nms/1_18_R2/src/main/java/com/hakan/core/hologram/line/entity/HHologramArmorStand_v1_18_R2.java
@@ -2,7 +2,6 @@ package com.hakan.core.hologram.line.entity;
 
 import com.hakan.core.HCore;
 import com.hakan.core.hologram.HHologram;
-import net.minecraft.core.Vector3f;
 import net.minecraft.network.chat.ChatMessage;
 import net.minecraft.network.protocol.game.PacketPlayOutEntityDestroy;
 import net.minecraft.network.protocol.game.PacketPlayOutEntityMetadata;
@@ -22,24 +21,25 @@ import java.util.Objects;
 /**
  * {@inheritDoc}
  */
-public class HHologramArmorStand_v1_18_R2 extends EntityArmorStand implements HHologramArmorStand {
+public class HHologramArmorStand_v1_18_R2 implements HHologramArmorStand {
 
     private final HHologram hologram;
+    private final EntityArmorStand armorStand;
 
     /**
      * {@inheritDoc}
      */
     private HHologramArmorStand_v1_18_R2(@Nonnull HHologram hHologram, @Nonnull Location location) {
-        super(((CraftWorld) Objects.requireNonNull(location.getWorld())).getHandle(), location.getX(), location.getY(), location.getZ());
-        super.persistentInvisibility = true; //set invisibility to true
-        super.b(5, true); //set invisibility to true
-        super.n(true); //set custom name visibility to true
-        super.t(true); //set marker to true
-        super.r(false); //set arms to false
-        super.s(true); //set no base-plate to true
-        super.e(true); //set no gravity to true
-        super.a(true); //set small to true
-        super.c(114.13f); //set health to 114.13 float
+        this.armorStand = new EntityArmorStand(((CraftWorld) Objects.requireNonNull(location.getWorld())).getHandle(), location.getX(), location.getY(), location.getZ());
+        this.armorStand.persistentInvisibility = true; //set invisibility to true
+        this.armorStand.b(5, true); //set invisibility to true
+        this.armorStand.n(true); //set custom name visibility to true
+        this.armorStand.t(true); //set marker to true
+        this.armorStand.r(false); //set arms to false
+        this.armorStand.s(true); //set no base-plate to true
+        this.armorStand.e(true); //set no gravity to true
+        this.armorStand.a(true); //set small to true
+        this.armorStand.c(114.13f); //set health to 114.13 float
         this.hologram = hHologram;
     }
 
@@ -49,7 +49,7 @@ public class HHologramArmorStand_v1_18_R2 extends EntityArmorStand implements HH
     @Nonnull
     @Override
     public String getText() {
-        return Objects.requireNonNull(super.Z()).a();
+        return Objects.requireNonNull(this.armorStand.Z()).a();
     }
 
     /**
@@ -59,10 +59,10 @@ public class HHologramArmorStand_v1_18_R2 extends EntityArmorStand implements HH
     public void setText(@Nonnull String text) {
         Validate.notNull(text, "text cannot be null");
 
-        super.a(new ChatMessage(text));
+        this.armorStand.a(new ChatMessage(text));
 
         HCore.sendPacket(this.hologram.getRenderer().getShownViewersAsPlayer(),
-                new PacketPlayOutEntityMetadata(super.ae(), super.ai(), true));
+                new PacketPlayOutEntityMetadata(this.armorStand.ae(), this.armorStand.ai(), true));
     }
 
     /**
@@ -71,7 +71,7 @@ public class HHologramArmorStand_v1_18_R2 extends EntityArmorStand implements HH
     @Nonnull
     @Override
     public Location getLocation() {
-        return super.getBukkitEntity().getLocation();
+        return this.armorStand.getBukkitEntity().getLocation();
     }
 
     /**
@@ -82,11 +82,11 @@ public class HHologramArmorStand_v1_18_R2 extends EntityArmorStand implements HH
         Validate.notNull(location, "location cannot be null");
 
         World world = ((CraftWorld) Objects.requireNonNull(location.getWorld())).getHandle();
-        if (!world.equals(super.s)) super.s = world;
-        super.a(location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch());
+        if (!world.equals(this.armorStand.s)) this.armorStand.s = world;
+        this.armorStand.a(location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch());
 
         HCore.sendPacket(this.hologram.getRenderer().getShownViewersAsPlayer(),
-                new PacketPlayOutEntityTeleport(this));
+                new PacketPlayOutEntityTeleport(this.armorStand));
     }
 
     /**
@@ -97,9 +97,9 @@ public class HHologramArmorStand_v1_18_R2 extends EntityArmorStand implements HH
         Validate.notNull(players, "players cannot be null");
 
         HCore.sendPacket(players,
-                new PacketPlayOutSpawnEntityLiving(this),
-                new PacketPlayOutEntityMetadata(super.ae(), super.ai(), true),
-                new PacketPlayOutEntityTeleport(this));
+                new PacketPlayOutSpawnEntityLiving(this.armorStand),
+                new PacketPlayOutEntityMetadata(this.armorStand.ae(), this.armorStand.ai(), true),
+                new PacketPlayOutEntityTeleport(this.armorStand));
     }
 
     /**
@@ -110,16 +110,6 @@ public class HHologramArmorStand_v1_18_R2 extends EntityArmorStand implements HH
         Validate.notNull(players, "players cannot be null");
 
         HCore.sendPacket(players,
-                new PacketPlayOutEntityDestroy(super.ae()));
-    }
-
-    @Override
-    public Vector3f x() {
-        return super.cj;
-    }
-
-    @Override
-    public boolean d_() {
-        return super.d_();
+                new PacketPlayOutEntityDestroy(this.armorStand.ae()));
     }
 }
