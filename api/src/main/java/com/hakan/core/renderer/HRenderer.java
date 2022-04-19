e74f7ae958655dd28a1a99b4b71bd8f11080635d
Osman Furkan <osmanfurkan115@gmail.com>
2022-04-19 21:52:57 +0300

    Use Java 8 stream api mapping

--- api/src/main/java/com/hakan/core/renderer/HRenderer.java
+++ api/src/main/java/com/hakan/core/renderer/HRenderer.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.Player;
 import javax.annotation.Nonnull;
 import java.util.*;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 /**
  * HRenderer class.
@@ -175,11 +176,10 @@ public final class HRenderer {
      */
     @Nonnull
     public List<Player> getShownViewersAsPlayer() {
-        List<Player> players = new ArrayList<>();
-        this.shownViewers.forEach(uid -> {
-            Player player = Bukkit.getPlayer(uid);
-            if (player != null) players.add(player);
-        });
+        List<Player> players = this.shownViewers.stream()
+                .map(Bukkit::getPlayer)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
         return players;
     }
 
@@ -322,11 +322,9 @@ public final class HRenderer {
     public Set<UUID> calculateViewers() {
         if (!this.showEveryone)
             return this.viewers;
-
-        Set<UUID> viewers = new HashSet<>();
-        this.location.getWorld().getPlayers()
-                .forEach(player -> viewers.add(player.getUniqueId()));
-        return viewers;
+        return location.getWorld().getPlayers().stream()
+                .map(Player::getUniqueId)
+                .collect(Collectors.toSet());
     }
 
     /**
