f62096a9898dddeec416d36170a85f7f941bc1c5
Hakan Kargın <hakankargin03@gmail.com>
2022-04-23 11:19:59 +0300

    HItemStack: - Add nbt supports for all versions. - Changed pom versions.

---
+++ nms/1_14_R1/src/main/java/com/hakan/core/item/nbt/HNbtManager_v1_14_R1.java
@@ -0,0 +1,92 @@
+package com.hakan.core.item.nbt;
+
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.server.v1_14_R1.MojangsonParser;
+import net.minecraft.server.v1_14_R1.NBTTagCompound;
+import org.bukkit.craftbukkit.v1_14_R1.inventory.CraftItemStack;
+import org.bukkit.inventory.ItemStack;
+
+import javax.annotation.Nonnull;
+import java.util.Objects;
+
+public class HNbtManager_v1_14_R1 implements HNbtManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Nonnull
+    @Override
+    public ItemStack set(@Nonnull ItemStack itemStack, @Nonnull String key, @Nonnull String value) {
+        Objects.requireNonNull(itemStack, "itemStack cannot be null");
+        Objects.requireNonNull(key, "key cannot be null");
+        Objects.requireNonNull(value, "value cannot be null");
+
+        net.minecraft.server.v1_14_R1.ItemStack nmsCopy = CraftItemStack.asNMSCopy(itemStack);
+
+        if (!nmsCopy.hasTag())
+            nmsCopy.setTag(new NBTTagCompound());
+
+        nmsCopy.getTag().setString(key, value);
+        return CraftItemStack.asBukkitCopy(nmsCopy);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Nonnull
+    @Override
+    public ItemStack set(@Nonnull ItemStack itemStack, @Nonnull String nbt) {
+        Objects.requireNonNull(itemStack, "itemStack cannot be null");
+        Objects.requireNonNull(nbt, "nbt cannot be null");
+
+        net.minecraft.server.v1_14_R1.ItemStack nmsCopy = CraftItemStack.asNMSCopy(itemStack);
+
+        if (!nmsCopy.hasTag())
+            nmsCopy.setTag(new NBTTagCompound());
+
+        nmsCopy.getTag().a(this.parse(nbt));
+        return CraftItemStack.asBukkitCopy(nmsCopy);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Nonnull
+    @Override
+    public String get(@Nonnull ItemStack itemStack, @Nonnull String key) {
+        Objects.requireNonNull(itemStack, "itemStack cannot be null");
+        Objects.requireNonNull(key, "key cannot be null");
+
+        net.minecraft.server.v1_14_R1.ItemStack nmsCopy = CraftItemStack.asNMSCopy(itemStack);
+
+        if (!nmsCopy.hasTag())
+            nmsCopy.setTag(new NBTTagCompound());
+
+        NBTTagCompound nbtTagCompound = nmsCopy.getTag();
+        return nbtTagCompound.hasKey(key) ? nbtTagCompound.getString(key) : "{}";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Nonnull
+    @Override
+    public String get(@Nonnull ItemStack itemStack) {
+        Objects.requireNonNull(itemStack, "itemStack cannot be null");
+
+        net.minecraft.server.v1_14_R1.ItemStack nmsCopy = CraftItemStack.asNMSCopy(itemStack);
+
+        if (!nmsCopy.hasTag())
+            nmsCopy.setTag(new NBTTagCompound());
+
+        return nmsCopy.getTag().toString();
+    }
+
+    private NBTTagCompound parse(String nbt) {
+        try {
+            return MojangsonParser.parse(nbt);
+        } catch (CommandSyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ nms/1_18_R2/src/main/java/com/hakan/core/item/nbt/HNbtManager_v1_18_R2.java
@@ -0,0 +1,92 @@
+package com.hakan.core.item.nbt;
+
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.nbt.MojangsonParser;
+import net.minecraft.nbt.NBTTagCompound;
+import org.bukkit.craftbukkit.v1_18_R2.inventory.CraftItemStack;
+import org.bukkit.inventory.ItemStack;
+
+import javax.annotation.Nonnull;
+import java.util.Objects;
+
+public class HNbtManager_v1_18_R2 implements HNbtManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Nonnull
+    @Override
+    public ItemStack set(@Nonnull ItemStack itemStack, @Nonnull String key, @Nonnull String value) {
+        Objects.requireNonNull(itemStack, "itemStack cannot be null");
+        Objects.requireNonNull(key, "key cannot be null");
+        Objects.requireNonNull(value, "value cannot be null");
+
+        net.minecraft.world.item.ItemStack nmsCopy = CraftItemStack.asNMSCopy(itemStack);
+
+        if (nmsCopy.t() == null)
+            nmsCopy.c(new NBTTagCompound());
+
+        nmsCopy.t().a(key, value);
+        return CraftItemStack.asBukkitCopy(nmsCopy);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Nonnull
+    @Override
+    public ItemStack set(@Nonnull ItemStack itemStack, @Nonnull String nbt) {
+        Objects.requireNonNull(itemStack, "itemStack cannot be null");
+        Objects.requireNonNull(nbt, "nbt cannot be null");
+
+        net.minecraft.world.item.ItemStack nmsCopy = CraftItemStack.asNMSCopy(itemStack);
+
+        if (nmsCopy.t() == null)
+            nmsCopy.c(new NBTTagCompound());
+
+        nmsCopy.t().a(this.parse(nbt));
+        return CraftItemStack.asBukkitCopy(nmsCopy);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Nonnull
+    @Override
+    public String get(@Nonnull ItemStack itemStack, @Nonnull String key) {
+        Objects.requireNonNull(itemStack, "itemStack cannot be null");
+        Objects.requireNonNull(key, "key cannot be null");
+
+        net.minecraft.world.item.ItemStack nmsCopy = CraftItemStack.asNMSCopy(itemStack);
+
+        if (nmsCopy.t() == null)
+            nmsCopy.c(new NBTTagCompound());
+
+        NBTTagCompound nbtTagCompound = nmsCopy.t();
+        return nbtTagCompound.e(key) ? nbtTagCompound.l(key) : "{}";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Nonnull
+    @Override
+    public String get(@Nonnull ItemStack itemStack) {
+        Objects.requireNonNull(itemStack, "itemStack cannot be null");
+
+        net.minecraft.world.item.ItemStack nmsCopy = CraftItemStack.asNMSCopy(itemStack);
+
+        if (nmsCopy.t() == null)
+            nmsCopy.c(new NBTTagCompound());
+
+        return nmsCopy.t().toString();
+    }
+
+    private NBTTagCompound parse(String nbt) {
+        try {
+            return MojangsonParser.a(nbt);
+        } catch (CommandSyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
