a15a4a959ef045e2a6e5ae1864ac774253cc29e1
Hakan Kargın <hakankargin03@gmail.com>
2022-04-23 10:39:27 +0300

    HItemStack: - Add nbt method to HItemStack class. - Add 1.8 support for nbt.

---
+++ nms/1_8_R3/src/main/java/com/hakan/core/item/nbt/HNbtManager_v1_8_R3.java
@@ -0,0 +1,92 @@
+package com.hakan.core.item.nbt;
+
+import net.minecraft.server.v1_8_R3.MojangsonParseException;
+import net.minecraft.server.v1_8_R3.MojangsonParser;
+import net.minecraft.server.v1_8_R3.NBTTagCompound;
+import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftItemStack;
+import org.bukkit.inventory.ItemStack;
+
+import javax.annotation.Nonnull;
+import java.util.Objects;
+
+public class HNbtManager_v1_8_R3 implements HNbtManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Nonnull
+    @Override
+    public ItemStack set(@Nonnull ItemStack itemStack, @Nonnull String key, @Nonnull String value) {
+        Objects.requireNonNull(itemStack, "itemStack cannot be null");
+        Objects.requireNonNull(key, "key cannot be null");
+        Objects.requireNonNull(value, "value cannot be null");
+
+        net.minecraft.server.v1_8_R3.ItemStack nmsCopy = CraftItemStack.asNMSCopy(itemStack);
+
+        if (!nmsCopy.hasTag())
+            nmsCopy.setTag(new NBTTagCompound());
+
+        nmsCopy.getTag().setString(key, value);
+        return CraftItemStack.asBukkitCopy(nmsCopy);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Nonnull
+    @Override
+    public ItemStack set(@Nonnull ItemStack itemStack, @Nonnull String nbt) {
+        Objects.requireNonNull(itemStack, "itemStack cannot be null");
+        Objects.requireNonNull(nbt, "nbt cannot be null");
+
+        net.minecraft.server.v1_8_R3.ItemStack nmsCopy = CraftItemStack.asNMSCopy(itemStack);
+
+        if (!nmsCopy.hasTag())
+            nmsCopy.setTag(new NBTTagCompound());
+
+        nmsCopy.getTag().a(this.parse(nbt));
+        return CraftItemStack.asBukkitCopy(nmsCopy);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Nonnull
+    @Override
+    public String get(@Nonnull ItemStack itemStack, @Nonnull String key) {
+        Objects.requireNonNull(itemStack, "itemStack cannot be null");
+        Objects.requireNonNull(key, "key cannot be null");
+
+        net.minecraft.server.v1_8_R3.ItemStack nmsCopy = CraftItemStack.asNMSCopy(itemStack);
+
+        if (!nmsCopy.hasTag())
+            nmsCopy.setTag(new NBTTagCompound());
+
+        NBTTagCompound nbtTagCompound = nmsCopy.getTag();
+        return nbtTagCompound.hasKey(key) ? nbtTagCompound.getString(key) : "{}";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Nonnull
+    @Override
+    public String get(@Nonnull ItemStack itemStack) {
+        Objects.requireNonNull(itemStack, "itemStack cannot be null");
+
+        net.minecraft.server.v1_8_R3.ItemStack nmsCopy = CraftItemStack.asNMSCopy(itemStack);
+
+        if (!nmsCopy.hasTag())
+            nmsCopy.setTag(new NBTTagCompound());
+
+        return nmsCopy.getTag().toString();
+    }
+
+    private NBTTagCompound parse(String nbt) {
+        try {
+            return MojangsonParser.parse(nbt);
+        } catch (MojangsonParseException e) {
+            return new NBTTagCompound();
+        }
+    }
+}
