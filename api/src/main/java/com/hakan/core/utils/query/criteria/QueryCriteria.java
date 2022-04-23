349809b5607d290672e502ac6fa0784b2c8761a1
Hakan Kargın <hakankargin03@gmail.com>
2022-04-23 17:20:06 +0300

    Add javadocs and annotations to query builder classes

--- api/src/main/java/com/hakan/core/utils/query/criteria/QueryCriteria.java
+++ api/src/main/java/com/hakan/core/utils/query/criteria/QueryCriteria.java
@@ -1,6 +1,9 @@
 package com.hakan.core.utils.query.criteria;
 
 public abstract class QueryCriteria {
+
     protected final StringBuilder criteria = new StringBuilder();
+
     public abstract String getCriteriaQuery();
+
 }
--- api/src/main/java/com/hakan/core/utils/query/criteria/where/WhereCriteria.java
+++ api/src/main/java/com/hakan/core/utils/query/criteria/where/WhereCriteria.java
@@ -7,29 +7,52 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Objects;
 
-public class WhereCriteria extends QueryCriteria {
+/**
+ * Where criteria class.
+ */
+public final class WhereCriteria extends QueryCriteria {
+
     private final Map<String, String> where;
 
+    /**
+     * Creates new instance of WhereCriteria class.
+     */
     public WhereCriteria() {
         this.where = new LinkedHashMap<>();
     }
 
-    public void add(@Nonnull String column, @Nonnull Object value) {
+    /**
+     * Adds new where criteria.
+     *
+     * @param column Column name.
+     * @param value  Value.
+     * @return Where criteria.
+     */
+    @Nonnull
+    public WhereCriteria add(@Nonnull String column, @Nonnull Object value) {
         Objects.requireNonNull(column, "column cannot be null!");
         Objects.requireNonNull(value, "value cannot be null!");
 
         String columnReplaced = column.replace(column, "`" + column + "`");
         String valueReplaced = value.toString().replace("'", "''");
         this.where.put(columnReplaced, valueReplaced.replace(valueReplaced, "'" + valueReplaced + "'"));
+        return this;
     }
 
+    /**
+     * Builds where criteria.
+     *
+     * @return Where criteria.
+     */
+    @Nonnull
     @Override
     public String getCriteriaQuery() {
         if (this.where.isEmpty()) return "";
 
         this.criteria.append(" WHERE ");
-        where.forEach((key, value) -> criteria.append(key).append(" = ").append(value).append(" AND "));
+        this.where.forEach((key, value) -> criteria.append(key).append(" = ").append(value).append(" AND "));
         this.criteria.delete(this.criteria.length() - 5, this.criteria.length());
+
         return this.criteria.toString();
     }
 }
