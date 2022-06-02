d4efccb552471f60a9297d7d4e6bde0afdde0e94
Hakan Kargın <hakankargin03@gmail.com>
2022-06-02 11:55:11 +0300

    Add database update system

---
+++ api/src/main/java/com/hakan/core/database/DatabaseHandler.java
@@ -0,0 +1,58 @@
+package com.hakan.core.database;
+
+import javax.annotation.Nonnull;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * DatabaseHandler class to
+ * handle database operations.
+ */
+@SuppressWarnings({"unchecked"})
+public final class DatabaseHandler {
+
+    private static final Map<Class<?>, DatabaseProvider<?>> providers = new HashMap<>();
+
+    /**
+     * Registers database provider.
+     *
+     * @param objectClass Database object class.
+     * @param provider    Database provider.
+     * @param <T>         Database object class.
+     */
+    @Nonnull
+    public static <T extends DatabaseObject> DatabaseProvider<T> registerProvider(@Nonnull Class<T> objectClass, @Nonnull DatabaseProvider<T> provider) {
+        Objects.requireNonNull(objectClass, "objectClass cannot be null!");
+        Objects.requireNonNull(provider, "provider cannot be null!");
+        providers.put(objectClass, provider);
+        return provider;
+    }
+
+    /**
+     * Finds database provider.
+     *
+     * @param objectClass Database object class.
+     * @param <T>         Database object class.
+     * @return Database provider.
+     */
+    @Nonnull
+    public static <T extends DatabaseObject> Optional<DatabaseProvider<T>> findProvider(@Nonnull Class<T> objectClass) {
+        Objects.requireNonNull(objectClass, "objectClass cannot be null!");
+        return providers.containsKey(objectClass) ? Optional.of((DatabaseProvider<T>) providers.get(objectClass)) : Optional.empty();
+    }
+
+    /**
+     * Gets database provider.
+     *
+     * @param objectClass Database object class.
+     * @param <T>         Database object class.
+     * @return Database provider.
+     */
+    @Nonnull
+    public static <T extends DatabaseObject> DatabaseProvider<T> getProvider(@Nonnull Class<T> objectClass) {
+        Objects.requireNonNull(objectClass, "objectClass cannot be null!");
+        return findProvider(objectClass).orElseThrow(() -> new IllegalArgumentException("no provider found for " + objectClass.getName()));
+    }
+}
--- api/src/main/java/com/hakan/core/database/DatabaseProvider.java
+++ api/src/main/java/com/hakan/core/database/DatabaseProvider.java
@@ -4,6 +4,7 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Database class to implement
@@ -12,12 +13,81 @@ import java.util.List;
  * @param <T> Database object class.
  */
 @SuppressWarnings({"unused"})
-public interface DatabaseProvider<T extends DatabaseObject> {
+public abstract class DatabaseProvider<T extends DatabaseObject> {
+
+    private final DatabaseUpdater<T> updater;
+
+    /**
+     * Database provider constructor.
+     */
+    public DatabaseProvider() {
+        this.updater = new DatabaseUpdater<>(this);
+    }
+
+    /**
+     * Gets database updater.
+     *
+     * @return Database updater.
+     */
+    @Nonnull
+    public final DatabaseUpdater<T> getUpdater() {
+        return this.updater;
+    }
+
+    /**
+     * Updates all objects in the database
+     * every selected time.
+     *
+     * @param async If true, updates will be done in a separate thread.
+     * @param time  Time to wait between updates.
+     * @param unit  Time unit.
+     * @return Instance of this class.
+     */
+    public final DatabaseProvider<T> updateEvery(boolean async, int time, @Nonnull TimeUnit unit) {
+        this.updater.updateEvery(async, time, unit);
+        return this;
+    }
+
+    /**
+     * Updates all objects in the database
+     * every selected time.
+     *
+     * @param time Time to wait between updates.
+     * @param unit Time unit.
+     * @return Instance of this class.
+     */
+    public final DatabaseProvider<T> updateEvery(int time, @Nonnull TimeUnit unit) {
+        this.updater.updateEvery(time, unit);
+        return this;
+    }
+
+    /**
+     * Adds an object to the database for updating.
+     *
+     * @param t Object to add.
+     * @return Instance of this class.
+     */
+    public final DatabaseProvider<T> addUpdateObject(T t) {
+        this.updater.add(t);
+        return this;
+    }
+
+    /**
+     * Removes an object from the database for updating.
+     *
+     * @param t Object to remove.
+     * @return Instance of this class.
+     */
+    public final DatabaseProvider<T> removeUpdateObject(T t) {
+        this.updater.remove(t);
+        return this;
+    }
+
 
     /**
      * Table create, yaml create, etc.
      */
-    void create();
+    public abstract void create();
 
     /**
      * Gets values.
@@ -25,7 +95,7 @@ public interface DatabaseProvider<T extends DatabaseObject> {
      * @return Values.
      */
     @Nonnull
-    List<T> getValues();
+    public abstract List<T> getValues();
 
     /**
      * Gets value from key and value.
@@ -35,47 +105,47 @@ public interface DatabaseProvider<T extends DatabaseObject> {
      * @return Value.
      */
     @Nullable
-    T getValue(@Nonnull String key, @Nonnull Object value);
+    public abstract T getValue(@Nonnull String key, @Nonnull Object value);
 
     /**
      * Inserts data to database.
      *
-     * @param t T
+     * @param t Database object.
      */
-    void insert(@Nonnull T t);
+    public abstract void insert(@Nonnull T t);
 
     /**
      * Saves data to database.
      *
-     * @param t T
+     * @param t Database object.
      */
-    void update(@Nonnull T t);
+    public abstract void update(@Nonnull T t);
 
     /**
      * Deletes data from database.
      *
-     * @param t T
+     * @param t Database object.
      */
-    void delete(@Nonnull T t);
+    public abstract void delete(@Nonnull T t);
 
     /**
      * Inserts data to database.
      *
-     * @param t T
+     * @param t Database object.
      */
-    void insert(@Nonnull Collection<T> t);
+    public abstract void insert(@Nonnull Collection<T> t);
 
     /**
      * Saves data to database.
      *
-     * @param t T
+     * @param t Database object.
      */
-    void update(@Nonnull Collection<T> t);
+    public abstract void update(@Nonnull Collection<T> t);
 
     /**
      * Deletes data from database.
      *
-     * @param t T
+     * @param t Database object.
      */
-    void delete(@Nonnull Collection<T> t);
+    public abstract void delete(@Nonnull Collection<T> t);
 }
