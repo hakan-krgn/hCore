a184e702469bb24a50ad4601ef91f8203d5e403e
Hakan Kargın <hakankargin03@gmail.com>
2022-06-08 15:07:46 +0300

    Fix hologram name problem for 1.19

--- nms/1_19_R1/src/main/java/com/hakan/core/hologram/line/entity/HHologramArmorStand_v1_19_R1.java
+++ nms/1_19_R1/src/main/java/com/hakan/core/hologram/line/entity/HHologramArmorStand_v1_19_R1.java
@@ -58,7 +58,7 @@ public final class HHologramArmorStand_v1_19_R1 implements HHologramArmorStand {
      */
     @Override
     public void setText(@Nonnull String text) {
-        this.armorStand.a(CraftChatMessage.fromStringOrNull(Objects.requireNonNull(text, "text cannot be null!")));
+        this.armorStand.b(CraftChatMessage.fromStringOrNull(Objects.requireNonNull(text, "text cannot be null!")));
         HCore.sendPacket(this.hologram.getRenderer().getShownViewersAsPlayer(),
                 new PacketPlayOutEntityMetadata(this.armorStand.ae(), this.armorStand.ai(), true));
     }
