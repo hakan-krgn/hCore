06de1342af16f724bc8332f8b7393cd984364ae6
Hakan Kargın <hakankargin03@gmail.com>
2022-06-08 10:28:03 +0300

    Add 1.19 classes

---
+++ nms/1_19_R1/src/main/java/com/hakan/core/message/title/HTitleHandler_v1_19_R1.java
@@ -0,0 +1,34 @@
+package com.hakan.core.message.title;
+
+import com.hakan.core.HCore;
+import net.minecraft.network.chat.IChatBaseComponent;
+import net.minecraft.network.protocol.game.ClientboundSetSubtitleTextPacket;
+import net.minecraft.network.protocol.game.ClientboundSetTitleTextPacket;
+import net.minecraft.network.protocol.game.ClientboundSetTitlesAnimationPacket;
+import org.bukkit.craftbukkit.v1_18_R2.util.CraftChatMessage;
+import org.bukkit.entity.Player;
+
+import javax.annotation.Nonnull;
+import java.util.Objects;
+
+/**
+ * {@inheritDoc}
+ */
+public final class HTitleHandler_v1_19_R1 implements HTitleHandler {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void send(@Nonnull Player player, @Nonnull HTitle hTitle) {
+        Objects.requireNonNull(player, "player cannot be null!");
+        Objects.requireNonNull(player, "hTitle class cannot be null!");
+
+        IChatBaseComponent titleString = CraftChatMessage.fromStringOrNull(hTitle.getTitle());
+        IChatBaseComponent subtitleString = CraftChatMessage.fromStringOrNull(hTitle.getSubtitle());
+
+        HCore.sendPacket(player, new ClientboundSetTitlesAnimationPacket(hTitle.getFadeIn(), hTitle.getStay(), hTitle.getFadeOut()));
+        HCore.sendPacket(player, new ClientboundSetTitleTextPacket(titleString));
+        HCore.sendPacket(player, new ClientboundSetSubtitleTextPacket(subtitleString));
+    }
+}
