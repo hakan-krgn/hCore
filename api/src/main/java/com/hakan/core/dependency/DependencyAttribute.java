119e23c5334b5c09ced7c74a8932856c07c70e16
Hakan Kargın <hakankargin03@gmail.com>
2022-10-05 16:25:28 +0300

    Dependency Loader: + Add auto dependency loader. + Add xml reader api. + Add save-path support to attribute. + Remove unnecessary methods from HCore and DependencyHandler class. + Rebuild dependency utils class.

--- api/src/main/java/com/hakan/core/dependency/DependencyAttribute.java
+++ api/src/main/java/com/hakan/core/dependency/DependencyAttribute.java
@@ -17,6 +17,7 @@ public final class DependencyAttribute {
     private final String artifactId;
     private final String version;
     private final String website;
+    private final String savePath;
 
     /**
      * Creates a new dependency attribute.
@@ -29,6 +30,7 @@ public final class DependencyAttribute {
         this.groupId = Validate.notNull(dependency.groupId(), "groupId cannot be null!");
         this.version = Validate.notNull(dependency.version(), "version cannot be null!");
         this.website = Validate.notNull(dependency.website(), "website cannot be null!");
+        this.savePath = Validate.notNull(dependency.savePath(), "savePath cannot be null!");
     }
 
     /**
@@ -42,11 +44,13 @@ public final class DependencyAttribute {
     public DependencyAttribute(@Nonnull String groupId,
                                @Nonnull String artifactId,
                                @Nonnull String version,
-                               @Nonnull String website) {
+                               @Nonnull String website,
+                               @Nonnull String savePath) {
         this.artifactId = Validate.notNull(artifactId, "artifactId cannot be null!");
         this.groupId = Validate.notNull(groupId, "groupId cannot be null!");
         this.version = Validate.notNull(version, "version cannot be null!");
         this.website = Validate.notNull(website, "website cannot be null!");
+        this.savePath = Validate.notNull(savePath, "save path cannot be null!");
     }
 
     /**
@@ -80,6 +84,16 @@ public final class DependencyAttribute {
     }
 
     /**
+     * Gets the save path.
+     *
+     * @return The save path.
+     */
+    @Nonnull
+    public String getSavePath() {
+        return this.savePath;
+    }
+
+    /**
      * Gets the website.
      *
      * @return The website.
@@ -90,28 +104,14 @@ public final class DependencyAttribute {
     }
 
     /**
-     * Gets the dependency name.
-     *
-     * @return The dependency name.
-     */
-    @Nonnull
-    public String asPath(@Nonnull File defaultFolder) {
-        Validate.notNull(defaultFolder, "default file cannot be null!");
-        return this.asPath(defaultFolder.getPath());
-    }
-
-    /**
      * Calculates save path from default path.
      * with dependency artifactId and version.
      *
-     * @param defaultPath The default path.
      * @return The save path.
      */
     @Nonnull
-    public String asPath(@Nonnull String defaultPath) {
-        Validate.notNull(defaultPath, "default path cannot be null!");
-
-        String path = (defaultPath.endsWith(File.separator)) ? defaultPath : defaultPath + File.separator;
+    public String asSavePath() {
+        String path = (this.savePath.endsWith(File.separator)) ? this.savePath : this.savePath + File.separator;
         if (this.website.endsWith(".jar"))
             return path + this.website.substring(this.website.lastIndexOf("/") + 1);
         return path + this.artifactId + "-" + this.version + ".jar";
@@ -124,8 +124,10 @@ public final class DependencyAttribute {
      * @return The URL of jar.
      */
     @Nonnull
-    public String asUrl() {
-        if (this.website.endsWith(".jar"))
+    public String asJarUrl() {
+        if (this.website.endsWith(".pom"))
+            return this.website.replace(".pom", ".jar");
+        else if (this.website.endsWith(".jar"))
             return this.website;
 
         String url = (this.website.endsWith("/")) ? this.website : this.website + "/";
@@ -134,5 +136,26 @@ public final class DependencyAttribute {
         String version = this.version;
 
         return url + groupId + "/" + artifactId + "/" + version + "/" + artifactId + "-" + version + ".jar";
+    }
+
+    /**
+     * Calculates URL from given groupId,
+     * artifactId, version and website.
+     *
+     * @return The URL of jar.
+     */
+    @Nonnull
+    public String asPomUrl() {
+        if (this.website.endsWith(".jar"))
+            return this.website.replace(".jar", ".pom");
+        else if (this.website.endsWith(".pom"))
+            return this.website;
+
+        String url = (this.website.endsWith("/")) ? this.website : this.website + "/";
+        String groupId = this.groupId.replace(".", "/");
+        String artifactId = this.artifactId;
+        String version = this.version;
+
+        return url + groupId + "/" + artifactId + "/" + version + "/" + artifactId + "-" + version + ".pom";
     }
 }
