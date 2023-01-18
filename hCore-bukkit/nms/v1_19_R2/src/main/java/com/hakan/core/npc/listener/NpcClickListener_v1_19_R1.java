9acc1944792f31da18ff490935e53b1c2ff679b6
Hakan Kargın <hakankargin03@gmail.com>
2023-01-18 12:28:36 +0300

    Add 1.19.3 support

---
+++ hCore-bukkit/nms/v1_19_R2/src/main/java/com/hakan/core/npc/listener/NpcClickListener_v1_19_R1.java
@@ -0,0 +1,30 @@
+package com.hakan.core.npc.listener;
+
+import com.hakan.core.npc.Npc;
+import com.hakan.core.npc.NpcHandler;
+import com.hakan.core.packet.event.PacketEvent;
+import org.bukkit.entity.Player;
+
+import javax.annotation.Nonnull;
+
+/**
+ * {@inheritDoc}
+ */
+public final class NpcClickListener_v1_19_R1 extends NpcClickListener {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onEntityInteractEvent(@Nonnull PacketEvent event, @Nonnull Player player) {
+        int id = event.getValue("a");
+
+        NpcHandler.findByEntityID(id).ifPresent(npc -> {
+            if (event.getValue("b").getClass().getName().equals("net.minecraft.network.protocol.game.PacketPlayInUseEntity$1")) {
+                npc.getAction().onClick(player, Npc.Action.LEFT_CLICK);
+            } else {
+                npc.getAction().onClick(player, Npc.Action.RIGHT_CLICK);
+            }
+        });
+    }
+}
