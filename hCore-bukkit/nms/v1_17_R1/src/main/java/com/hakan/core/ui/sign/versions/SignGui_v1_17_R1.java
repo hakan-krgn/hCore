a1a0fbef1ec31999ac111746c44371426751ba9e
Hakan Kargın <hakankargin03@gmail.com>
2023-09-04 12:11:03 +0300

    change sign creation location for sign gui

--- hCore-bukkit/nms/v1_17_R1/src/main/java/com/hakan/core/ui/sign/versions/SignGui_v1_17_R1.java
+++ hCore-bukkit/nms/v1_17_R1/src/main/java/com/hakan/core/ui/sign/versions/SignGui_v1_17_R1.java
@@ -38,8 +38,10 @@ public final class SignGui_v1_17_R1 extends SignGui {
     @Nonnull
     @Override
     public SignGui open() {
-        Location location = super.player.getLocation();
-        BlockPosition blockPosition = new BlockPosition(location.getBlockX(), LOWEST_Y_AXIS + 1, location.getBlockZ());
+        Location location = super.player.getEyeLocation();
+        location.clone().add(location.getDirection().multiply(-3));
+
+        BlockPosition blockPosition = new BlockPosition(location.getBlockX(), location.getBlockY(), location.getBlockZ());
         IBlockData data = CraftMagicNumbers.getBlock(super.type.asMaterial()).getBlockData();
 
         HCore.sendPacket(super.player, new PacketPlayOutBlockChange(blockPosition, data));
