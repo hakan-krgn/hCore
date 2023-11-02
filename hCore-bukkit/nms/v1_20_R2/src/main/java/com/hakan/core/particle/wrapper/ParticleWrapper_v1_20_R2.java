d8c99b0542868528bc2a784db2887db94a3f9fa1
Hakan Kargın <hakankargin03@gmail.com>
2023-11-02 20:30:58 +0300

    fix NoSuchMethodException issue in ParticleWrapper_v1_20_R2

--- hCore-bukkit/nms/v1_20_R2/src/main/java/com/hakan/core/particle/wrapper/ParticleWrapper_v1_20_R2.java
+++ hCore-bukkit/nms/v1_20_R2/src/main/java/com/hakan/core/particle/wrapper/ParticleWrapper_v1_20_R2.java
@@ -1,12 +1,8 @@
 package com.hakan.core.particle.wrapper;
 
-import com.hakan.core.HCore;
 import com.hakan.core.particle.Particle;
 import com.hakan.core.utils.Validate;
-import net.minecraft.core.particles.ParticleParam;
-import net.minecraft.network.protocol.game.PacketPlayOutWorldParticles;
 import org.bukkit.Location;
-import org.bukkit.craftbukkit.v1_20_R2.CraftParticle;
 import org.bukkit.entity.Player;
 
 import javax.annotation.Nonnull;
@@ -25,19 +21,25 @@ public final class ParticleWrapper_v1_20_R2 implements ParticleWrapper {
         Validate.notNull(location, "location cannot be null!");
         Validate.notNull(particle, "particle class cannot be null!");
 
-        ParticleParam particleParam = null;
+        org.bukkit.Particle particleParam = null;
         try {
-            particleParam = CraftParticle.toNMS(org.bukkit.Particle.valueOf(particle.getName()));
+            particleParam = org.bukkit.Particle.valueOf(particle.getName());
         } catch (Exception e) {
             for (org.bukkit.Particle _particle : org.bukkit.Particle.values()) {
                 if (_particle.name().toLowerCase().contains(particle.getName().toLowerCase())) {
-                    particleParam = CraftParticle.toNMS(_particle);
+                    particleParam = _particle;
                     break;
                 }
             }
         }
 
         Validate.notNull(particleParam, "particle couldn't find!");
-        HCore.sendPacket(player, new PacketPlayOutWorldParticles(particleParam, false, (float) location.getX(), (float) location.getY(), (float) location.getZ(), (float) particle.getOffset().getX(), (float) particle.getOffset().getY(), (float) particle.getOffset().getZ(), (float) particle.getSpeed(), particle.getAmount()));
+        player.spawnParticle(particleParam, location,
+                particle.getAmount(),
+                particle.getOffset().getX(),
+                particle.getOffset().getY(),
+                particle.getOffset().getZ(),
+                particle.getSpeed()
+        );
     }
 }
