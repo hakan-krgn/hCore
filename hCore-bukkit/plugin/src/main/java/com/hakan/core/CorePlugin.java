48b5f25df3d4669c9ebe38b64ba6dce213b93a1d
Hakan Kargın <hakankargin03@gmail.com>
2023-12-20 19:01:49 +0300

    upgrade java and project versions

--- hCore-bukkit/plugin/src/main/java/com/hakan/core/CorePlugin.java
+++ hCore-bukkit/plugin/src/main/java/com/hakan/core/CorePlugin.java
@@ -6,7 +6,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 @Plugin(
         name = "hCore",
         authors = "Hakan",
-        version = "0.7.2.7",
+        version = "0.7.2.8",
         description = "A plugin which contains hCore.",
         website = "https://github.com/hakan-krgn/hCore"
 )
--- hCore-bungee/plugin/src/main/java/com/hakan/core/CorePlugin.java
+++ hCore-bungee/plugin/src/main/java/com/hakan/core/CorePlugin.java
@@ -5,7 +5,7 @@ import com.hakan.core.plugin.Plugin;
 @Plugin(
         name = "hCore",
         authors = "Hakan",
-        version = "0.7.2.7",
+        version = "0.7.2.8",
         description = "A plugin which contains hCore.",
         website = "https://github.com/hakan-krgn/hCore"
 )
